87b7286d8cf0d92cfb21a55d3aacc6f2459f0f35
Nhi Mai Thi Quynh <nhi.mai@news.com.au>
2018-06-29 14:37:39 +0700

    test

--- city.js
+++ city.js
@@ -1,24 +1,19 @@
+const EventsEmmit = require('events')
 
-var Events = require('events')
+let myPackage = new class extends EventsEmmit{
+     readData(countries, counter = 0) {
+        let returnedData = {};
+        var length = countries.length;
 
-let counter = 0;
-
-let myPackage = new class extends Events{
-     readData(fileJSON) {
-        var returnedData = {};
-        var length = fileJSON.length;
     if (counter < length) {
         setTimeout(() => {
-            returnedData=fileJSON[counter];
+            returnedData = countries[counter];
             returnedData.index = counter;
             this.emit('countrydata', returnedData);
             counter++;
-            this.readData(fileJSON,counter);
+            this.readData(countries,counter);
         }, 1000);
     }
  }
 }
 module.exports = myPackage;
-
-
-
